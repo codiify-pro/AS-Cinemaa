30c24af1d085923e135b5f4b7515da0143ac332f
Codiify <aaryansah954@gmail.com>
2026-04-01 22:56:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,10 +95,10 @@ LANDSCAPE_POSTER = bool(environ.get('LANDSCAPE_POSTER', True)) # Shows landscape
 # ============================
 # Verification Settings
 # ============================
-IS_VERIFY = is_enabled('IS_VERIFY', False)  # Verification On (True) / Off (False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)  # Verification On (True) / Off (False)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002521835919')) #Verification Channel Id 
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002521835919')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://adespresso.com/wp-content/uploads/2022/09/how-to-get-verified-on-facebook.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/How_to_get_AS_Links/6")   # Tutorial link for verification
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/How_to_get_AS_Links/6")   # Second tutorial link for verification
